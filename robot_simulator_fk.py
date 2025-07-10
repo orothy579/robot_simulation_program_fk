22e18db8bf6acfce3db64efd2eac12020385c407
orothy579 <ftsky2050@naver.com>
2025-07-10 23:41:52 +0900

    v0

--- robot_simulator_fk.py
+++ robot_simulator_fk.py
@@ -1,5 +1,5 @@
-# 예시 코드
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def dh_transform(theta, d, a, alpha):
@@ -25,3 +25,25 @@ def forward_kinematics(theta_list, a, d, alpha):
         T = T @ T_i  # Combine transformations
         positions.append(T[:3, 3])  # Append the position of the end effector
     return T, positions
+
+
+def visualize_robot(pos):
+    pos = np.array(pos)
+    xs, ys, zs = pos[:, 0], pos[:, 1], pos[:, 2]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(xs, ys, zs, '-o', linewidth=2)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.title("3D Visualization of Robotic Arm")
+    plt.show()
+
+
+theta = [0, 0, 0, 0, 0, 0]
+a = [0, -0.425, -0.392, 0, 0, 0]
+d = [0.089, 0, 0, 0.109, 0.095, 0.082]
+alpha = [np.pi/2, 0, 0, np.pi/2, -np.pi/2, 0]
+
+T, ee_pos = forward_kinematics(theta, a, d, alpha)
+visualize_robot(ee_pos)
